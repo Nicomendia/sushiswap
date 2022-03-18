7dfa7f8917c00423e72f1e62e44ae5c1b8e34e6b
Nico M <nico.mendia@gmail.com>
2022-03-18 16:51:30 -0400

    2nd version with withdraw funds

--- contracts/SimpleWallet.sol
+++ contracts/SimpleWallet.sol
@@ -7,29 +7,25 @@ import './uniswapv2/interfaces/IUniswapV2Factory.sol';
 import './uniswapv2/interfaces/IUniswapV2Router02.sol';
 
 import './uniswapv2/interfaces/IERC20.sol';
-import "hardhat/console.sol";
-
-//import './uniswapv2/libraries/SafeMath.sol';
+import "./Ownable.sol";
 
 interface IMasterChef {
   function deposit(uint256 _pid, uint256 _amount) external;
+  function withdraw(uint256 _pid, uint256 _amount) external;
 }
 
-contract SimpleWallet {
-
-    //using SafeMathUniswap for uint;
-
+contract SimpleWallet is Ownable {
     address public immutable factory;
     address public immutable router;
     address public immutable chef;
+    address public immutable sushi;
 
-    constructor(address _router, address _chef) public {
+    constructor(address _router, address _chef, address _sushi) public {
         router = _router;
         chef = _chef;
+        sushi = _sushi;
         factory = IUniswapV2Router02(_router).factory();
     }
-
-    // TODO: add ownable & withdraw
 
     // Adds liquidity and deposits LP tokens in only one step 
     function addLiquidityWithFarming(
@@ -41,7 +37,7 @@ contract SimpleWallet {
         uint _amountBMin,
         uint _deadline,
         uint _masterChefPoolId
-    ) external {
+    ) external onlyOwner {
         uint amountA;
         uint amountB;
         uint liquidity;
@@ -57,8 +53,29 @@ contract SimpleWallet {
         IMasterChef(chef).deposit(_masterChefPoolId, liquidity);
     }
 
+    // withdraws original deposited tokens and all sushi rewards
+    function withdrawLiquidityWithRewards(
+        address _tokenA,
+        address _tokenB,
+        uint _liquidity,
+        uint _amountAMin,
+        uint _amountBMin,
+        uint _deadline,
+        uint _masterChefPoolId,
+        uint _masterChefPoolAmount
+    ) external onlyOwner {
+        IERC20Uniswap(sushi).transfer(msg.sender, IERC20Uniswap(sushi).balanceOf(address(this)));
+ 
+        IMasterChef(chef).withdraw(_masterChefPoolId, _masterChefPoolAmount);
+
+        address pair = IUniswapV2Factory(factory).getPair(_tokenA, _tokenB);
+        IERC20Uniswap(pair).approve(router, _liquidity);
+        (uint amountA, uint amountB) = IUniswapV2Router02(router).removeLiquidity(_tokenA, _tokenB, _liquidity, _amountAMin, _amountBMin, msg.sender, _deadline);
+        
+    }
+
     // updates sushi rewards for farming pool
-    function updateRewards(uint _poolId) external {
+    function updateRewards(uint _poolId) external onlyOwner {
         IMasterChef(chef).deposit(_poolId, 0);
     }
 }
